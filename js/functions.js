50a1e3f6ebcd15360b77fa1f0af22964496d135d
Tommi Kivimäki <tommi@tommikivimaki.com>
2018-03-13 20:04:15 +0200

    Added full bleed support for .aligncenter images with captions

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,6 @@
+// Add class for .aligncenter images so that they can be targeted in scss
+
 (function($) {
+  $('figure.wp-caption.aligncenter').removeAttr('style');
   $('img.aligncenter').wrap('<figure class="centered-image" />');
 }) (jQuery);
